ecf943a503282b469c076f1f226fbd968e5dce84
hawsatit <hawsatit@grinnell.edu>
2025-11-05 16:55:25 -0600

    Add files via upload

---
+++ src/main/java/edu/grinnell/psychMAP25/AuditoryPresentation/AuditoryPresentation.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+package edu.grinnell.psychMAP25.AuditoryPresentation;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+import javax.swing.JFrame;
+
+
+
+public class AuditoryPresentation {
+    public static void main(String[] args) {
+        System.out.println("AuditoryPresentation running...");
+        // Your logic here
+    }
+}
+
---
+++ src/main/java/edu/grinnell/psychMAP25/AuditoryPresentation/TonePlayer.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.grinnell.psychMAP25.AuditoryPresentation;
+import java.util.List;
+
+
+public class TonePlayer {
+
+
+}
